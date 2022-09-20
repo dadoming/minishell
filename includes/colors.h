0234833756723e175174c2c039c2bebba91f18ea
Davz <davidmdg98@hotmail.com>
2022-09-20 12:46:03 +0100

    color lib

---
+++ includes/colors.h
@@ -0,0 +1,17 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+
+
+// Regular bold
+#define BBLK "\e[1;30m"
+#define BRED "\e[1;31m"
+#define BGRN "\e[1;32m"
+#define BYEL "\e[1;33m"
+#define BBLU "\e[1;34m"
+#define BMAG "\e[1;35m"
+#define BCYN "\e[1;36m"
+#define BWHT "\e[1;37m"
+#define COLOR_RESET "\e[0m"
+
+#endif
