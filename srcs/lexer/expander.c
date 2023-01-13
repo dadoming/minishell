2830633c71ee62904a426d50d4ca55af482ed998
Davz <davidmdg98@hotmail.com>
2023-01-13 00:42:44 +0000

    added possible implementation

--- srcs/lexer/expander.c
+++ srcs/lexer/expander.c
@@ -1,34 +1,104 @@
 #include "../../includes/minishell.h"
 
-static int check_quote(int *active_quote, char c);
-char* expand_dollars(char *content);
-void expand_values(void *token);
-char *replace(char *if_this_has, char *this, char *str_to_replace, int active_quote);
-//static char* copy_until(char *str, int n);
+/*
+Here is an example of how you might send the address 
+of a string variable, change the string, and then free the old string in C:
 
-void expander(void)
-{
-    list()->_iterator(mini()->arg_list, expand_values);
-    helper_print();
+#include <stdio.h>
+#include <stdlib.h>
+
+void changeString(char** str) {
+    char* old_str = *str;
+    *str = strdup("new string");
+    free(old_str);
 }
 
-void expand_values(void *token)
+int main() {
+    char* my_string = strdup("old string");
+    printf("Original string: %s\n", my_string);
+    changeString(&my_string);
+    printf("New string: %s\n", my_string);
+    free(my_string);
+    return 0;
+}
+
+
+
+*/
+static int check_quote(int *active_quote, char c)
 {
-    char *content;
+    if (*active_quote == NO_QUOTE)
+    {
+        if (c == '\'')
+            *active_quote = SINGLE_QUOTE;
+        else if (c == '\"')
+            *active_quote = DOUBLE_QUOTE;
+        else
+            *active_quote = NO_QUOTE;
+    }
+    else if (*active_quote == SINGLE_QUOTE)
+    {
+        if (c == '\'')
+            *active_quote = NO_QUOTE;
+        else
+            *active_quote = SINGLE_QUOTE;
+    }
+    else if (*active_quote == DOUBLE_QUOTE)
+    {
+        if (c == '\"')
+            *active_quote = NO_QUOTE;
+        else
+            *active_quote = DOUBLE_QUOTE;
+    }
+    return (*active_quote);
+}
+
+
+char *expand_environment(char **content)
+{
+    char *ret;
+
+    ret = *content;
+    ret = "LOL";
+    printf("content: %s\n", ret);
+    return (ret);
+}
+
+char* expand_dollars(char *content, int i)
+{
+    char *ret;
+
+    ret = content;
+    if (ret[i + 1] == '?')
+    {
+        printf("$? is used to find the return value of the last executed command.\n");
+        return (ret);
+    }
+    if (check()->_is_alnum(ret[i + 1]) == 1)
+    {
+        ret = expand_environment(&ret);
+        return (ret);
+    }
+    return (NULL);
+}
+
+void expand_values(char **token)
+{
     int i;
     int active_quote;
 
-    content = token;
     active_quote = NO_QUOTE;
     i = 0;
-    while (content[i] != '\0')
+    while (*token[i] != '\0')
     {   
-        check_quote(&active_quote, content[i]);
-        if (content[i] == '$')
+        check_quote(&active_quote, *token[i]);
+        if (*token[i] == '$')
         {
             if (active_quote == DOUBLE_QUOTE || active_quote == NO_QUOTE)
             {
-                expand_dollars(&content);
+                *token = expand_dollars(*token, i);
+                i = 0;
+                active_quote = NO_QUOTE;
                 break;
             }
             else
@@ -41,10 +111,29 @@ void expand_values(void *token)
     }
 }
 
-char* expand_dollars(char *content)
+void expander(void)
 {
-    
+    t_list *aux;
+
+    aux = mini()->arg_list;
+    while (aux != NULL)
+    {
+        expand_values(&aux->token);
+        aux = aux->next;
+    }
+    helper_print();
 }
+
+
+
+/*
+    Handle environment variables ($ followed by a sequence of characters)
+    Handle $? which should expand to the exit status of the most recently executed foreground pipeline
+
+                    This pointer needs to be freed
+                          |                       */
+
+
 
 
 /*
@@ -168,30 +257,3 @@ char *expand_dollar(char **arg_pointer, int checkpoint)
 }
 */
 
-static int check_quote(int *active_quote, char c)
-{
-    if (*active_quote == NO_QUOTE)
-    {
-        if (c == '\'')
-            *active_quote = SINGLE_QUOTE;
-        else if (c == '\"')
-            *active_quote = DOUBLE_QUOTE;
-        else
-            *active_quote = NO_QUOTE;
-    }
-    else if (*active_quote == SINGLE_QUOTE)
-    {
-        if (c == '\'')
-            *active_quote = NO_QUOTE;
-        else
-            *active_quote = SINGLE_QUOTE;
-    }
-    else if (*active_quote == DOUBLE_QUOTE)
-    {
-        if (c == '\"')
-            *active_quote = NO_QUOTE;
-        else
-            *active_quote = DOUBLE_QUOTE;
-    }
-    return (*active_quote);
-}
