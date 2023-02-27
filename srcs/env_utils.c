df4845e7ad202bcd4d03164cd4b06dfed934ff32
Ruben Gomes <90207277+RubenJPG@users.noreply.github.com>
2023-02-27 16:15:44 +0000

    Norm

--- srcs/env_utils.c
+++ srcs/env_utils.c
@@ -1,22 +1,23 @@
 #include "../includes/minishell.h"
 
-// If env has value then return value else return NULL. Example: get_env(env, "PATH")
-char *get_env(char **env, char *var_name)
+// If env has value then return value else \
+NULL. Example: get_env(env, "PATH")
+char	*get_env(char **env, char *var_name)
 {
-    int i;
-    int len;
+	int	i;
+	int	len;
 
-    len = 0;
-    i = 0;
-    while (env[i])
-    {
-        len = string()->_length(var_name);
-        if ((string()->_compare_n(env[i], var_name, len) == 0) && \
-            (string()->_length_until_c(env[i], '=') == len))
-        {
-            return (string()->_duplicate(env[i] + len));
-        }
-        i++;
-    }
-    return (NULL);
+	len = 0;
+	i = 0;
+	while (env[i])
+	{
+		len = string()->_length(var_name);
+		if ((string()->_compare_n(env[i], var_name, len) == 0) && \
+			(string()->_length_until_c(env[i], '=') == len))
+		{
+			return (string()->_duplicate(env[i] + len));
+		}
+		i++;
+	}
+	return (NULL);
 }
