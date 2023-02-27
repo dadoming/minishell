e8a6746cd7b42e143632538e01145615f3e4865d
Ruben Gomes <90207277+RubenJPG@users.noreply.github.com>
2023-02-27 16:13:36 +0000

    Norm

--- srcs/close_program.c
+++ srcs/close_program.c
@@ -1,48 +1,50 @@
 #include "../includes/minishell.h"
 
-void free_array(char **array)
+void	free_array(char **array)
 {
-    int i = 0;
+	int	i;
 
-    if (array == NULL)
-        return ;
-    while (array[i])
-    {
-        free(array[i]);
-        i++;
-    }
-    free(array);
+	i = 0;
+	if (array == NULL)
+		return ;
+	while (array[i])
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
 }
 
-static void clear_core(shell_t *mini);
+static void	clear_core(shell_t *mini);
 
 /* Clears all memory created by the program, still leaves readline's
-    library allocated memory reachable but with no leaks. */
-void close_program(shell_t *mini)
+	library allocated memory reachable but with no leaks. */
+void	close_program(shell_t *mini)
 {
-    clear_looped_values(mini);
-    clear_core(mini);
-    rl_erase_empty_line = 1;
-    rl_clear_history();
-    free(mini);
-    return ;
+	clear_looped_values(mini);
+	clear_core(mini);
+	rl_erase_empty_line = 1;
+	rl_clear_history();
+	free(mini);
+	return ;
 }
 
-static void clear_core(shell_t *mini)
+/*if (mini()->core->prompt != NULL)free(mini()->core->prompt);*/
+static void	clear_core(shell_t *mini)
 {
-    if (mini->arg_list != NULL)
-        free_list(&mini->arg_list);
-    //if (mini()->core->prompt != NULL)
-      //   free(mini()->core->prompt);
-    int i = 0;
-    if(mini->core->env_p != NULL)
-    {
-        while (mini->core->env_p[i])
-        {
-            free(mini->core->env_p[i]);
-            i++;
-        }
-        free(mini->core->env_p);
-    }
-    free(mini->core);
+	int	i;
+
+	if (mini->arg_list != NULL)
+		free_list(&mini->arg_list);
+	i = 0;
+	if (mini->core->env_p != NULL)
+	{
+		while (mini->core->env_p[i])
+		{
+			free(mini->core->env_p[i]);
+			i++;
+		}
+		free(mini->core->env_p);
+	}
+	free(mini->core);
 }
