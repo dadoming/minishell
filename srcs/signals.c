304f1d7f32ad7f15b414a05581739fdbaad65b53
Ruben Gomes <90207277+RubenJPG@users.noreply.github.com>
2023-03-14 13:17:38 +0000

    Removed sigquit_handler

--- srcs/signals.c
+++ srcs/signals.c
@@ -2,13 +2,6 @@
 
 extern int g_exit_status;
 
-void sig_block(int signo)
-{
-    (void)signo;
-    signal(SIGINT, SIG_IGN);
-    g_exit_status = 130;
-}
-
 void sig_block_c(int signo)
 {
     (void)signo;
@@ -27,13 +20,6 @@ void sigint_handler(int signum)
 		rl_replace_line("", 0);
 		rl_redisplay();
 	}
-}
-
-void sigquit_handler(int signum)
-{
-    (void) signum;
-    rl_replace_line("", 0);
-	rl_redisplay();
 }
 
 void sigterm_handler(int signum)
